b574f2bf192a3063673334e7821e70bbc56acaf7
Raphaël <raphael.mabille@epitech.eu>
2025-12-23 11:54:28 +0000

    disconnect user when the lobby creator quit the lobby

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -9,16 +9,25 @@ extends Control
 @onready var vbox_create_lobby: VBoxContainer = $menu_buttons/vbox_create_lobby
 @onready var vbox_join_lobby: VBoxContainer = $menu_buttons/vbox_join_lobby
 @onready var la_you: Label = $menu_buttons/vbox_create_lobby/la_you
+@onready var vbox_in_remote_lobby: VBoxContainer = $menu_buttons/vbox_in_remote_lobby
 #-------------------------------------------------------------#
 
+enum State {
+	DISCONNECTED,
+	CONNECTING,
+	CONNECTED,
+	LOBBY_CREATOR
+}
 
-
-func _on_ready() -> void:
-	setCurrent(vbox_root)
+var state : State = State.DISCONNECTED
 
 var adress : String = "127.0.0.1"
 
 var peer_id_list = [1]
+
+func _on_ready() -> void:
+	setCurrent(vbox_root)
+
 
 #---------------------- button navigation --------------------#
 
@@ -37,6 +46,20 @@ func _on_bt_back_pressed() -> void:
 
 	if history.size() <= 1 :
 		bt_back.disabled = true
+	
+	if state == State.LOBBY_CREATOR :
+		Lobby.player_connected.disconnect(add_player_name)
+		Lobby.player_disconnected.disconnect(remove_player_name)
+		Lobby.stop_game()
+		for id in peer_id_list :
+			if id == 1 :
+				continue
+			remove_player_name(id)
+		state = State.DISCONNECTED
+	elif state == State.CONNECTING :
+		_on_bt_cancel_join_pressed()
+	elif state == State.CONNECTED :
+		disconect_from_server()
 
 #-------------------------------------------------------------#
 
@@ -84,6 +107,7 @@ func _on_bt_create_lobby_pressed() -> void:
 	Lobby.player_disconnected.connect(remove_player_name)
 	Lobby.create_game()
 	setCurrent(vbox_create_lobby)
+	state = State.LOBBY_CREATOR
 
 
 func _on_bt_join_lobby_pressed() -> void:
@@ -97,22 +121,47 @@ func _on_le_ip_adress_text_changed(new_text: String) -> void:
 @onready var as_loading: AnimatedSprite2D = $menu_buttons/vbox_join_lobby/as_loading
 @onready var cr_separator_2: ColorRect = $menu_buttons/vbox_join_lobby/cr_separator_2
 @onready var bt_cancel_join: Button = $menu_buttons/vbox_join_lobby/bt_cancel_join
+@onready var la_lobby_host_name: Label = $menu_buttons/vbox_in_remote_lobby/la_lobby_host_name
 
-func _on_bt_join_game_pressed() -> void:
-	var error = Lobby.join_game(adress)
-	as_loading.show()
-	as_loading.play("default")
-	cr_separator_2.show()
-	bt_cancel_join.show()
-	await Lobby.player_connected;
-	print("error : ", error)
-
-
-func _on_bt_cancel_join_pressed() -> void:
+func reset_connection_ui() -> void:
 	as_loading.hide()
 	as_loading.stop()
 	cr_separator_2.hide()
 	bt_cancel_join.hide()
+
+func add_host_name_ui(peer_id, player_info) -> void:
+	if peer_id == 1:
+		la_lobby_host_name.text = player_info.name
+
+func game_canceled(peer_id) -> void:
+	if peer_id == 1:
+		state = State.DISCONNECTED
+		Lobby.player_connected.disconnect(add_host_name_ui)
+		Lobby.player_disconnected.disconnect(game_canceled)
+		_on_bt_back_pressed()
+
+func _on_bt_join_game_pressed() -> void:
+	as_loading.show()
+	as_loading.play("default")
+	cr_separator_2.show()
+	bt_cancel_join.show()
+	Lobby.player_connected.connect(add_host_name_ui)
+	Lobby.player_disconnected.connect(game_canceled)
+	Lobby.join_game(adress)
+	state = State.CONNECTING
+	await Lobby.player_connected;
+	state = State.CONNECTED
+	reset_connection_ui()
+	setCurrent(vbox_in_remote_lobby)
+
+func disconect_from_server() -> void:
+	state = State.DISCONNECTED
 	Lobby.remove_multiplayer_peer()
+	Lobby.player_connected.disconnect(add_host_name_ui)
+	Lobby.player_disconnected.disconnect(game_canceled)
+	
+func _on_bt_cancel_join_pressed() -> void:
+	disconect_from_server()
+	reset_connection_ui()
 
 #-------------------------------------------------------------#
