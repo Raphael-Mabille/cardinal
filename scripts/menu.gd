8383e812d1403e48b44c9c40097979811d9bac90
Raphaël <raphael.mabille@epitech.eu>
2025-12-23 10:48:26 +0000

    add anim to join loading and disconect option

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -9,8 +9,8 @@ extends Control
 @onready var vbox_create_lobby: VBoxContainer = $menu_buttons/vbox_create_lobby
 @onready var vbox_join_lobby: VBoxContainer = $menu_buttons/vbox_join_lobby
 @onready var la_you: Label = $menu_buttons/vbox_create_lobby/la_you
-
 #-------------------------------------------------------------#
+
 
 
 func _on_ready() -> void:
@@ -64,15 +64,24 @@ func add_player_name_to_ui(p_name) -> void:
 	new_label.text = "    - " + p_name
 	la_you.add_sibling(new_label)
 
+func remove_player_name_to_ui(index) -> void:
+	var player_label = vbox_create_lobby.get_child(index + 1)
+	player_label.queue_free()
 
 func add_player_name(peer_id, player_info) -> void:
 	if peer_id_list.find(peer_id) == -1 :
 		peer_id_list.append(peer_id)
 		add_player_name_to_ui(player_info.name)
 
+func remove_player_name(peer_id) -> void:
+	print("player disconected")
+	var index = peer_id_list.find(peer_id)
+	peer_id_list.pop_at(index)
+	remove_player_name_to_ui(index)
 
 func _on_bt_create_lobby_pressed() -> void:
 	Lobby.player_connected.connect(add_player_name)
+	Lobby.player_disconnected.connect(remove_player_name)
 	Lobby.create_game()
 	setCurrent(vbox_create_lobby)
 
@@ -85,12 +94,25 @@ func _on_le_ip_adress_text_changed(new_text: String) -> void:
 	adress = new_text
 
 
-func player_connect(a, b) -> void:
-	print("peer_id, player_info", a, b)
+@onready var as_loading: AnimatedSprite2D = $menu_buttons/vbox_join_lobby/as_loading
+@onready var cr_separator_2: ColorRect = $menu_buttons/vbox_join_lobby/cr_separator_2
+@onready var bt_cancel_join: Button = $menu_buttons/vbox_join_lobby/bt_cancel_join
 
 func _on_bt_join_game_pressed() -> void:
-	Lobby.player_connected.connect(player_connect)
 	var error = Lobby.join_game(adress)
+	as_loading.show()
+	as_loading.play("default")
+	cr_separator_2.show()
+	bt_cancel_join.show()
+	await Lobby.player_connected;
 	print("error : ", error)
+
+
+func _on_bt_cancel_join_pressed() -> void:
+	as_loading.hide()
+	as_loading.stop()
+	cr_separator_2.hide()
+	bt_cancel_join.hide()
+	Lobby.remove_multiplayer_peer()
 
 #-------------------------------------------------------------#
